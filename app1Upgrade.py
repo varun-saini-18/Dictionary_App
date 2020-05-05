2cab17698ce561f2e364cd7abe5ad2f26b5b1c3f
Varun Saini <varunsaini1203@gmail.com>
2020-05-05 15:19:05 +0530

    First Commit

---
+++ app1Upgrade.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "ardit700_student",
+host = "108.167.140.122",
+database = "ardit700_pm1database"
+)
+
+cursor = con.cursor()
+
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = 'car' ")
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    print("No words found")
